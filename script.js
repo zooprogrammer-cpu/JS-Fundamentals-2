a834f3bb4f0164d3f581ea10493b3f64a909e649
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2022-08-25 16:25:28 -0500

    coding challenge 4 for loop

--- script.js
+++ script.js
@@ -1,33 +1,28 @@
+//coding challenge #4
 
-//Coding Challenge #3
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52]
+const tips =[]
+const totals=[]
 
+const calcTip = function (bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+  }
 
-let bmi 
-const mark ={
-    fullName : 'Mark Lawton',
-    mass: 78,
-    height: 1.69,
-    calcBMI : function(){
-        return this.mass / this.height **2
-    } 
+for (let i=0;i<bills.length;i++){
+    tips.push(calcTip(bills[i]))
+    totals.push(bills[i] + tips[i])
+}  
 
+// console.log(tips)
+console.log(totals)
+
+const calcAverage = function(arr){
+    let sum = 0
+    for(let j=0;j<arr.length;j++){
+        sum += arr[j]
+    }
+    const average = sum/arr.length
+    console.log(average)
 }
 
-const john = {
-    fullName: 'John Smith',
-    mass : 92,
-    height : 1.95,
-    calcBMI : function(){
-        return this.mass / this.height **2
-    } 
-}
-
-
-
-
-if (mark.calcBMI() > john.calcBMI()){
-    console.log(`${mark.fullName} has a higher BMI of ${(mark.calcBMI()).toFixed(2)}`)
-} else{
-    console.log(`John has a higher BMI of ${(john.calcBMI()).toFixed(2)}`)
-}
-
+calcAverage(totals)
